683d56bd7faa64215de2ca9d83b16e670374342d
supportdcbindia <support@dcbindia.in>
2025-11-03 13:31:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -198,7 +198,7 @@
                         <div class="container">
                             <div class="hm-inquire-content text-center fadeUp">
                                 <h2 class="text-white mb-50">Let’s connect and build better airflow together.</h2>
-                                <a class="click1" href="javascript:;" class="btn-theme-two">Inquire
+                                <a href="javascript:;" class="btn-theme-two click1">Inquire
                                     Now</a>
 
                             </div>
